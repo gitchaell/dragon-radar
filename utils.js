3f1754165eecf26e785ee611a8473a4ec1f04d73
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-12-10 23:14:03 -0400

    rotate z when change mobile orientation

--- utils.js
+++ utils.js
@@ -12,6 +12,31 @@ function randomLongitude() {
   return random({ minNumber: -180, maxNumber: 180, decimalPlaces: 6 });
 }
 
+
+function eulerFromQuaternion({ x, y, z, w }) {
+
+  let degree = 180 / Math.PI;
+  let roll = Math.atan2(2 * (w * x + y * z), w * w - x * x - y * y + z * z);
+  let pitch = Math.asin(2 * (w * y - x * z));
+  let yaw = Math.atan2(2 * (w * z + x * y), w * w + x * x - y * y - z * z);
+
+  if (pitch === + Math.PI / 2) {
+    roll = 0;
+    yaw = 2 * Math.atan2(x, w);
+  }
+
+  if (pitch === - Math.PI / 2) {
+    roll = 0;
+    yaw = -2 * Math.atan2(x, w);
+  }
+
+  return {
+    alpha: (yaw * degree).toFixed(2),
+    beta: (roll * degree).toFixed(2),
+    gamma: (pitch * degree).toFixed(2),
+  };
+}
+
 let screenWidth = screen.width;
 let screenHeight = screen.height;
 let ballDiameter = 16;
